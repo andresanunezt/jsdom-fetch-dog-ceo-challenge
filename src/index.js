b4af8c9fefe3678dcb0fc6a09aee1a6567a59cf7
Andrés Nuñez <andresanunez98@gmail.com>
2021-06-02 14:57:17 -0400

    commit tree e5b449266a0f59acb190a0d38b74b237ea2a0785

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('%c HI', 'color: firebrick')
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+fetch(imgUrl).then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log(json)
+});
